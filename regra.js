38f9abdb3bdb88919a10c0ac2e9c5054ede499b9
zorzal <50125344+zorzal@users.noreply.github.com>
2019-05-13 16:20:34 -0300

    v1.0

---
+++ regra.js
@@ -0,0 +1,73 @@
+function calcular() {
+
+    //LIMPA REGRAS DE FORMULÁRIO INVÁLIDO SEMPRE QUE A FUNÇÃO É CHAMADA
+    document.getElementById('dataHelp').classList.add('invisible');
+    document.getElementById('datafim').classList.remove('is-invalid'); 
+
+    semana = ["Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado", "Domingo"];
+    mes = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
+    console.log(document.getElementById("datainicio").value);
+
+    var dataInicio = new Date(document.getElementById("datainicio").value);
+    var dataFim = new Date(document.getElementById("datafim").value);
+    var diffMilissegundos = dataFim - dataInicio;
+    var diffSegundos = diffMilissegundos / 1000;
+    var diffMinutos = diffSegundos / 60;
+    var diffHoras = diffMinutos / 60;
+    var diffDias = diffHoras / 24;
+    var diffMeses = diffDias / 30; 
+
+    //dataInicio = dataInicio.toLocaleDateString('pt-BR');
+
+    console.log(dataInicio);
+
+    //VERIFICA SE A DATA FINAL É MAIOR QUE DATA INICIAL
+    if (diffMilissegundos < 0) {
+        document.getElementById('dataHelp').classList.remove('invisible');
+        document.getElementById('datafim').classList.add('is-invalid');    
+    }else{
+        document.getElementById('msgDataInicial').innerHTML = 'Data Inicial: '+ semana[dataInicio.getDay()] + ', '+ (dataInicio.getDate()+1) + ' de ' + mes[dataInicio.getMonth()] + ' de '+ dataInicio.getFullYear();
+        document.getElementById('msgDataFinal').innerHTML = 'Data Final: '+ semana[dataFim.getDay()] + ', '+ (dataFim.getDate()+1) + ' de ' + mes[dataFim.getMonth()] + ' de '+ dataFim.getFullYear();
+        document.getElementById('msg').innerHTML =  diffDias+' dias de diferênça!';
+    }
+
+    console.log(semana[dataInicio.getDay()]); //Retona o dia da semana
+    console.log(mes[dataInicio.getMonth()]); //Retona o mês
+    console.log('Diferença em dias '+diffDias);
+    console.log('Diferença em milesegundos '+diffMilissegundos);
+}      
+
+
+/*
+
+// Obtém a data/hora atual
+var data = new Date();
+
+// Guarda cada pedaço em uma variável
+var dia     = data.getDate();           // 1-31
+var dia_sem = data.getDay();            // 0-6 (zero=domingo)
+var mes     = data.getMonth();          // 0-11 (zero=janeiro)
+var ano2    = data.getYear();           // 2 dígitos
+var ano4    = data.getFullYear();       // 4 dígitos
+var hora    = data.getHours();          // 0-23
+var min     = data.getMinutes();        // 0-59
+var seg     = data.getSeconds();        // 0-59
+var mseg    = data.getMilliseconds();   // 0-999
+var tz      = data.getTimezoneOffset(); // em minutos
+
+// Formata a data e a hora (note o mês + 1)
+var str_data = dia + '/' + (mes+1) + '/' + ano4;
+var str_hora = hora + ':' + min + ':' + seg;
+
+// Mostra o resultado
+alert('Hoje é ' + str_data + ' às ' + str_hora + ' ' + dia_sem);
+
+
+
+var data = new Date();
+var dias = ['domingo','segunda','terça','quarta','quinta','sexta','sábado'];
+
+alert('Hoje é ' + dias[data.getDay()]);
+
+*/
